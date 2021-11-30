31fc6b47d05c6e9f3e38c9720c73e76cd4123876
ADMValverde <89872460+ADMValverde@users.noreply.github.com>
2021-11-29 22:47:00 -0800

    shouldve been updating
    I did a significant portion of the work and am now graphing. I forgot to save and commit on git, and I am gravely disapointed at myself for it.

--- lab.R
+++ lab.R
@@ -1,7 +1,7 @@
 #' ---
-#' title: "Data Science Methods, Lab for Week 08"
-#' author: "Your Name"
-#' email: Your Email
+#' title: "Data Science Methods, Lab for Week 06"
+#' author: "Aramis D. M. Valverde"
+#' email: amunoz-valverde@ucmerced.edu
 #' output:
 #'   html_document:
 #'     toc: true
@@ -11,11 +11,15 @@
 #' 
 #' *Specifically, we'll be exploring the relationship between case counts and mobility in the context of the Summer 2020 wave.* 
 #' 
-#' *This lab reproduces some of my own EDA of Covid-19 data from Summer 2020, in a more streamlined fashion.  Because it's still significantly longer and more complex than our other labs, I decided not to include much open-ended exploration.  As a result this might not feel very "exploratory," but it does do some other valuable things.  I encourage you to do spend a few hours exploring these data on your own, chasing after your own questions.*
+#' *This lab reproduces some of my own EDA of Covid-19 data from Summer 2020, in a more streamlined fashion.  Because it's still significantly longer and more complex than our *
+#' *other labs, I decided not to include much open-ended exploration.  As a result this might not feel very "exploratory," but it does do some other valuable things.  I encourage *
+#' *you to do spend a few hours exploring these data on your own, chasing after your own questions.*
 #' 
 
 #' # Reflexivity #
-#' *Before starting the lab, spend 3 minutes writing a response to each reflexivity question.  Use a timer.  Answer these questions off the top of your head: don't worry about consulting or citing outside sources or about getting the answers "right" or "wrong."* 
+#' *Before starting the lab, spend 3 minutes writing a response to each reflexivity question.  Use a timer.  Answer these questions off the top of your head: don't worry about *
+#' *consulting or citing outside sources or about getting the answers "right" or "wrong."* 
+#' 
 #' 1. *What do I already know about this subject?*
 #' 2. *Why am I studying this?*
 #' 3. *What do I expect or hope to find/learn, and why?*
@@ -26,6 +30,7 @@
 ## Setup ----
 ## **IMPORTANT**: Add all dependencies to `DESCRIPTION`
 ## Note:  I'm copying the relevant setup from class.  `covid_df` includes both the new cases/deaths per day as well as the rates per 100,000 residents. 
+
 library(tidyverse)
 library(tidylog)
 library(skimr)
@@ -80,12 +85,19 @@ dataf |>
 #' 
 
 #' 1. *For the county-level cumulative data, are there any limitations or caveats that we should keep in mind for our analysis?* 
-#' 
-#' 
+#' There are many, the case reporting is not normalized per county, as each county has different reporting standards. Cases were also reported at different rates, 
+#' and by differing avenues, meaning that spikes reported at one week may have happened weeks before, or may be artificial spikes or ruts due to case reporting that is 
+#' plainly inconsistent. 
+#' There is also the issue that labs are closed on weekends, meaning that reports of covid nearly always fall at the weekends. This is very obvious, yet much reporting at the 
+#' early pandemic tried to say that every weekend dip indicated the prompt end of the pandemic. The painful obviousness of the labs being closed and that driving weekend covid 
+#' numbers down drove me mad at the beginning of the pandemic, because; 1. There is no way that the people writing these stories were so oblivious, meaning that there were likely
+#' ulterior motives, and 2. this created an inconsitency that people would latch onto and which predictably damaged later discourse (see also the blatant lies about masks being 
+#' detrimental early in the pandemic.)
 #' 
 
 #' 2. *How about the Google Mobility data?* 
-#' 
+#' Some of the privacy protections could be an issue, especially in places with smaller populations. Also this is alphabet/google/android data, so it excluded people who arent
+#' users of those (primarally iphone users). There is a page of limitations on the site, however for our purposes those are the main issues.
 #' 
 
 
@@ -96,23 +108,80 @@ dataf |>
 #' *The original dataset is huge.  The full version is very large, so I've included a prefiltered version in `data` that's limited to California.  It's already been loaded above as `mob_df`.*  
 #' 1. *How many distinct values of `type` are there?  What does this variable indicate?*  
 #' 
+#' residential, workplace, retail, grocery, parks, and transit. it indicates the following, "
+#' The data shows how visitors to (or time spent in) categorized places change compared to our 
+#' baseline days. A baseline day represents a normal value for that day of the week. The baseline 
+#' day is the median value from the 5‑week period Jan 3 – Feb 6, 2020."-Google
 #' 
-#' 
+
+# Misc Code
+#test1 <- group_by('census_fips_code')
+
+#count(mob_df, variable_name='census_fips_code')
+#count(mob_df[c('census_fips_code')])
+
 
 #' 2. *How about `pct_diff`?* 
 #' 
-#' 
-#' 
-
-#' 3. *During our time period of interest, does `mob_df` contain mobility data for every county in California?  If some counties are missing data, which ones are they?  Hints: There are 58 counties in California.  Try counting and then filtering to identify counties with outlying row counts.*  
-#' 
-#' 
+#' This is difference compared to the normal baseline day, which is calculates as a median value from the period of jan 3rd. to feb 6th, 2020.
 #' 
 
-#' 4. *In the `plots` folder, take a look at `mobility.png`.  Recreate this plot.  (Use whatever theme and colors that you like.  To create a horizontal line: `geom_hline(yintercept = 0, alpha = .5)`.  You don't need to save to disk.)* 
+#' 3. *During our time period of interest, does `mob_df` contain mobility data for every county in California?  If some counties are missing data, which ones are they?*
+#'    *Hints: There are 58 counties in California.  Try counting and then filtering to identify counties with outlying row counts.*  
+#' There are 56 fips codes and 57 place ids based on the skim below. sub_region_2 also holds 56 variables, meaning that there are 2 missing counties, probably the tiny ones.
 #' 
 
-#' 5. *Again, the standard narrative of Covid-19 in California says that people were staying home in the spring, then going out more in May-June as stay-at-home orders were lifted.  Do this data support that narrative?*
+skim(mob_df)
+
+
+
+#' 4. *In the `plots` folder, take a look at `mobility.png`.  Recreate this plot.  (Use whatever theme and colors that you like. *
+#'    *To create a horizontal line: `geom_hline(yintercept = 0, alpha = .5)`.  You don't need to save to disk.)* 
+#' 
+
+# df_bmss <- filter(mob_df, cut == 'Merced County', 'Butte County', 'Sacramento County', 'Santa Clara County')
+
+#regionsUsed <- c('Merced County', 'Butte County', 'Sacramento County', 'Santa Clara County')
+#df_bmss <- filter(mob_df, sub_region_2 %in% regionsUsed)
+
+#ggplot(data = df_bmss, mapping = aes(x = date, y = pct_diff)) +
+  #  geom_point() +
+  #  geom_line(color = "blue")
+
+###AV create seperate df for each county, will combine plots later
+    
+df_butte <- filter(mob_df, sub_region_2 == 'Butte County')
+
+df_merced <- filter(mob_df, sub_region_2 == 'Merced County')
+
+df_sacramento <- filter(mob_df, sub_region_2 == 'Sacramento County')
+
+df_santaclara <- filter(mob_df, sub_region_2 == 'Santa Clara County')
+
+###AV create plots per county (I know this isn't efficient and will not be the same, however I think it may be more instructive for me to go this route than with a 
+###AV facet wrap that I don't quite get yet)
+
+ggplot(data = df_butte, mapping = aes(x = date, y = pct_diff)) +
+  geom_hline(yintercept = 0, alpha = .5) +
+  geom_point() +
+  geom_line(color = "blue")
+
+
+ggplot(data = df_butte, mapping = aes(x = date, y = pct_diff, color = type)) 
+geom_line(color = type)
+
+df_butte %/%
+    ggplot(aes(date, pct_diff)) + 
+    geom_line(color = "blue")
+
+ggplot(data = df_butte, mapping = aes(x = date, y = pct_diff)) +
+    geom_hline(yintercept = 0, alpha = .5) +
+    geom_line(aes(y = parks), color = "red") + 
+    geom_line(aes(y = residential), color="green", linetype="twodash") 
+
+
+#' 5. *Again, the standard narrative of Covid-19 in California says that people were staying home in the spring, then going out more in May-June as stay-at-home orders were lifted.  *
+#'    *Do this data support that narrative?*
 #'   
 #'   
 #'   
@@ -125,11 +194,19 @@ dataf |>
 
 #' Problem 3
 # problem 3 ----
-#' *Our specific interest in this data is the relationship between the relaxation of stay-at-home rules in June and the outbreak that peaked in July.  The standard narrative of the summer outbreak focuses on the kind of individual behavior measured by the Google Mobility data.  According to this narrative, individuals became more relaxed about staying at home and maintaining social distancing in June, and this caused an increase of cases starting 2-4 weeks later and peaking in July.  Media coverage focused especially on parks, as in this _San Francisco Chronicle_ story: <https://www.sfchronicle.com/bayarea/article/Bay-Area-residents-mostly-wear-masks-and-follow-15452707.php>*. 
+#' *Our specific interest in this data is the relationship between the relaxation of stay-at-home rules in June and the outbreak that peaked in July.  *
+#' *The standard narrative of the summer outbreak focuses on the kind of individual behavior measured by the Google Mobility data.  *
+#' *According to this narrative, individuals became more relaxed about staying at home and maintaining social distancing in June, and this caused an *
+#' *increase of cases starting 2-4 weeks later and peaking in July.  Media coverage focused especially on parks, as in this *
+#' *_San Francisco Chronicle_ story: <https://www.sfchronicle.com/bayarea/article/Bay-Area-residents-mostly-wear-masks-and-follow-15452707.php>*. 
 #' 
-#' *We'll investigate this by taking month-long averages for social distancing in June (as measured by the `parks` type in the mobility data) and Covid-19 cases and deaths in July, then drawing a scatterplot.*
+#' *We'll investigate this by taking month-long averages for social distancing in June (as measured by the `parks` type in the mobility data) and *
+#' *Covid-19 cases and deaths in July, then drawing a scatterplot.*
 #' 
-#' *(Just an aside.  Most presentations of Covid-19 data use 7-day rolling averages.  Either they don't show raw counts at all, or they emphasize the rolling averages rather than the raw counts.  In the `plots` folder, `chronicle.png` shows an example from the _San Francisco Chronicle_.  Because this lab is already super long and complicated, I decided to skip the rolling averages.  Two common packages for calculating rolling averages are (`zoo`)[https://cran.r-project.org/web/packages/zoo/index.html] and (`slider`)[https://cran.r-project.org/web/packages/slider/].)*
+#' *(Just an aside.  Most presentations of Covid-19 data use 7-day rolling averages.  Either they don't show raw counts at all, or they emphasize the *
+#' *rolling averages rather than the raw counts.  In the `plots` folder, `chronicle.png` shows an example from the _San Francisco Chronicle_.  Because this *
+#' *lab is already super long and complicated, I decided to skip the rolling averages.  Two common packages for calculating rolling averages are (`zoo`)*
+#' *[https://cran.r-project.org/web/packages/zoo/index.html] and (`slider`)[https://cran.r-project.org/web/packages/slider/].)*
 #' 
 
 #' 1. *This is just one way we could get at the relationship between stay-at-home in June and the peak of the outbreak in July.  What are some other approaches we might take?*
@@ -137,7 +214,8 @@ dataf |>
 #' 
 #' 
 
-#' 2. *Construct a dataframe `parks_june` that reports the mean level of "parks" mobility for each county in June 2020.  Call the variable `parks`.  (Just so the automatic checks know where to look.)*
+#' 2. *Construct a dataframe `parks_june` that reports the mean level of "parks" mobility for each county in June 2020.  Call the variable `parks`.  *
+#' *(Just so the automatic checks know where to look.)*
 #' 
 #' *Hints:*
 #' 
@@ -147,13 +225,16 @@ dataf |>
 #' - And it should have one row for each county in the mobility data for which we have an estimate for "parks".*
 #' 
 
-#' 3. *Construct a dataframe `cases_july` that reports the total level of new cases per 1 million residents of each county in July 2020.  (Don't worry about negative values.  I'm just asking you to do `sum(cases_per_pop)`.)  This dataframe should have three columns and one row for each county in the Covid-19 data.*
+#' 3. *Construct a dataframe `cases_july` that reports the total level of new cases per 1 million residents of each county in July 2020.  (Don't worry about negative values.  *
+#' *I'm just asking you to do `sum(cases_per_pop)`.)  This dataframe should have three columns and one row for each county in the Covid-19 data.*
 #' 
 
-#' 4. *Combine `parks_june` with `cases_july` using an inner join and appropriate matching columns.  Assign the result to `summer_df`.  (Note that the automatic checks will be looking at the `county` column.)* 
+#' 4. *Combine `parks_june` with `cases_july` using an inner join and appropriate matching columns.  Assign the result to `summer_df`.  *
+#' *(Note that the automatic checks will be looking at the `county` column.)* 
 #' 
 
-#' 5. *Construct a scatterplot of July cases against June "parks."  The standard narrative suggests that there should be a positive correlation between these variables: as people spent more time at parks in June, this led to more cases in July.  Does the scatterplot support this?* 
+#' 5. *Construct a scatterplot of July cases against June "parks."  The standard narrative suggests that there should be a positive correlation *
+#' *between these variables: as people spent more time at parks in June, this led to more cases in July.  Does the scatterplot support this?* 
 #' 
 #' 
 #' 
@@ -164,9 +245,15 @@ dataf |>
 #  problem 4 ----
 #' *We'll be discussing this part in class.*
 #' 
-#' *If you haven't done so yet, read Rex Douglass' blog post "How to be Curious Instead of Contrarian About COVID-19" (<https://rexdouglass.github.io/TIGR/Douglass_2020_How_To_Be_Curious_Instead_of_Contrarian_About_Covid19.nb.html>).  Douglass presents eight "lessons" for non-epidemiologists who are working with epidemiological data.  I assume you're not an epidemiologist.  If you are an epidemiologist, please pretend that you're not for the sake of this exercise.*
+#' *If you haven't done so yet, read Rex Douglass' blog post "How to be Curious Instead of Contrarian About COVID-19" *
+#' *(<https://rexdouglass.github.io/TIGR/Douglass_2020_How_To_Be_Curious_Instead_of_Contrarian_About_Covid19.nb.html>).  *
+#' *Douglass presents eight "lessons" for non-epidemiologists who are working with epidemiological data.  I assume you're not an epidemiologist.  *
+#' *If you are an epidemiologist, please pretend that you're not for the sake of this exercise.*
 #' 
-#' *Suppose you were exploring Covid data for fun in late July or early August 2020 (cf this blog post by Kieran Healy: <https://kieranhealy.org/blog/archives/2020/05/21/the-kitchen-counter-observatory/>), and that you found the pattern that we identified in Problem 3.  You're contemplating writing a blog post, op-ed, or even short research letter to share this finding.  How can Douglass' 8 lessons help us determine whether and how to share this finding?* 
+#' *Suppose you were exploring Covid data for fun in late July or early August 2020 (cf this blog post by Kieran Healy: *
+#' *<https://kieranhealy.org/blog/archives/2020/05/21/the-kitchen-counter-observatory/>), and that you found the pattern that we identified in Problem 3.  *
+#' *You're contemplating writing a blog post, op-ed, or even short research letter to share this finding.  How can Douglass' 8 lessons help us determine *
+#' *whether and how to share this finding?* 
 #' 
 #' 
 #' 
